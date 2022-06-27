5cf9714c6b43e917fce857e597296040129daea5
Ivan Fedchenko <paladin7701@gmail.com>
2022-06-27 09:58:14 +0200

    Дз4
    Доработал дз для отправки

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    [SerializeField] private GameObject firstScreen;
+    [SerializeField] private GameObject secondScreen;
+    [SerializeField] private GameObject thirdScreen;
+    private GameObject currentScreen;
+    private void Start()
+    {
+        firstScreen.SetActive(true);
+        currentScreen = firstScreen;
+    }
+    public void ChangeState(GameObject state)
+    {
+        if (currentScreen != null)
+        {
+            currentScreen.SetActive(false);
+            state.SetActive(true);
+            currentScreen = state;
+        }
+    }
+}
---
+++ Assets/Scripts/TwoNumbersComparer.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TwoNumbersComparer : MonoBehaviour
+{
+    [SerializeField] public InputField numComparer1;
+    [SerializeField] public InputField numComparer2;
+    [SerializeField] private Text resultComparer;
+    int numCom1;
+    int numCom2;
+    private void Start()
+    {
+        
+    }
+    
+    private void onConvertNum() //метод для конвертации введеных чисел, из текста в числа
+    {
+        numCom1 = Convert.ToInt32(numComparer1.text);
+        numCom2 = Convert.ToInt32(numComparer2.text);
+    }
+    public void readText()
+    {
+        onConvertNum();
+        if (numCom1 > numCom2)
+        {
+            resultComparer.text = $"Число {numCom1} больше {numCom2}";
+        }
+        else if (numCom1 < numCom2)
+        {
+            resultComparer.text = $"Число {numCom1} меньше {numCom2}";
+        }
+        else if (numCom1 == numCom2)
+        {
+            resultComparer.text = "Числа равны";
+        }
+    }
+}
